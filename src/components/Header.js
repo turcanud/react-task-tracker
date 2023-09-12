0f3c67b65987b92c6e5b06414f350b3c9913a6b9
Țurcanu Dorin <93262227+turcanud@users.noreply.github.com>
2023-09-12 21:34:34 +0300

    adv

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
+import PropTypes from 'prop-types'
 
-function Header() {
+function Header({title}) {
   return (
-    <header>
-        <h1>Task Tracker</h1>
+    <header className='header'>
+        <h1>{title}</h1>
+        <button className='btn'>Add</button>
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+ 
+Header.propTypes = {
+  title: PropTypes.string,
 }
 
 export default Header
